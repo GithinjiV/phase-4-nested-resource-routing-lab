4cf90e5a03e14b9f4827c0af20b84585fde274e6
vinnie <githinjigicheru146@gmail.com>
2024-05-04 22:07:24 +0300

    Nested routes for users with items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def render_record_not_found
+    render json:{error: "User not found"}, status: :not_found
   end
 
 end
